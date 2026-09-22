e396a47f48856f1e62adf06c7c4b4bf0e836ea19
Mykola Bezruchko <hronoaza00@gmail.com>
2026-09-22 14:08:12 +0300

    test: complete D9 compile-fail contract

---
+++ apps/soam-runtime/tests/compile_fail/d9_construct_stream_instance_id_from_bytes.cpp
@@ -0,0 +1,6 @@
+#include "policy_persistence.hpp"
+int main() {
+    AdaptiveMesh::PersistenceStreamInstanceId::Bytes bytes{};
+    AdaptiveMesh::PersistenceStreamInstanceId value{bytes};
+    (void)value;
+}
