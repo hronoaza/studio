4d165b902e34f477957092c25af3733667b14c1d
Mykola Bezruchko <hronoaza00@gmail.com>
2026-09-22 14:53:16 +0300

    test: prove PRESERVE cannot become transition direction

---
+++ apps/soam-transition-request/tests/compile_fail/preserve_to_requested_direction.cpp
@@ -0,0 +1,9 @@
+#include "production_transition_request.hpp"
+
+int main() {
+    AdaptiveMesh::PersistentBridgeRecommendation recommendation =
+        AdaptiveMesh::PersistentBridgeRecommendation::PRESERVE;
+    AdaptiveMesh::RequestedTransitionDirection direction = recommendation;
+    (void)direction;
+    return 0;
+}
